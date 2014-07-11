f8184a7846fcbfcc8e2603744f8da8fb11018af9
Soren Hansen <Soren.Hansen@ril.com>
2014-07-11 09:56:10 +0200

    Add arguments for configuring Keystone credentials
    Change-Id: I5523633c68a4e24829024f02937418b3c64df32f

--- manifests/api.pp
+++ manifests/api.pp
@@ -35,13 +35,58 @@
 #   Should be an valid interger
 #   Defaults to '1000'.
 #
+# [*auth_host*]
+#   (optional) The IP of the server running keystone
+#   Defaults to '127.0.0.1'
+#
+# [*auth_port*]
+#   (optional) The port to use when authenticating against Keystone
+#   Defaults to 35357
+#
+# [*auth_protocol*]
+#   (optional) The protocol to use when authenticating against Keystone
+#   Defaults to 'http'
+#
+# [*auth_uri*]
+#   (optional) The uri of a Keystone service to authenticate against
+#   Defaults to false
+#
+# [*auth_admin_prefix*]
+#   (optional) Prefix to prepend at the beginning of the keystone path
+#   Defaults to false
+#
+# [*auth_version*]
+#   (optional) API version of the admin Identity API endpoint
+#   for example, use 'v3.0' for the keystone version 3.0 api
+#   Defaults to false
+#
+# [*admin_tenant_name*]
+#   (optional) The name of the tenant to create in keystone for use by the ironic services
+#   Defaults to 'services'
+#
+# [*admin_user*]
+#   (optional) The name of the user to create in keystone for use by the ironic services
+#   Defaults to 'ironic'
+#
+# [*admin_password*]
+#   (required) The password to set for the ironic admin user in keystone
+#
 
 class ironic::api (
-  $package_ensure = 'present',
-  $enabled        = true,
-  $host_ip        = '0.0.0.0',
-  $port           = '6385',
-  $max_limit      = '1000'
+  $package_ensure    = 'present',
+  $enabled           = true,
+  $host_ip           = '0.0.0.0',
+  $port              = '6385',
+  $max_limit         = '1000',
+  $auth_host         = '127.0.0.1',
+  $auth_port         = 35357,
+  $auth_protocol     = 'http',
+  $auth_uri          = false,
+  $auth_admin_prefix = false,
+  $auth_version      = false,
+  $admin_tenant_name = 'services',
+  $admin_user        = 'ironic',
+  $admin_password,
 ) {
 
   include ironic::params
@@ -79,5 +124,38 @@ class ironic::api (
     hasstatus => true,
     require   => Class['api'],
   }
+
+  if $auth_uri {
+    ironic_config { 'keystone_authtoken/auth_uri': value => $auth_uri; }
+  } else {
+    ironic_config { 'keystone_authtoken/auth_uri': value => "${auth_protocol}://${auth_host}:5000/"; }
+  }
+
+  if $auth_version {
+    ironic_config { 'keystone_authtoken/auth_version': value => $auth_version; }
+  } else {
+    ironic_config { 'keystone_authtoken/auth_version': ensure => absent; }
+  }
+
+  ironic_config {
+    'keystone_authtoken/auth_host':         value => $auth_host;
+    'keystone_authtoken/auth_port':         value => $auth_port;
+    'keystone_authtoken/auth_protocol':     value => $auth_protocol;
+    'keystone_authtoken/admin_tenant_name': value => $admin_tenant_name;
+    'keystone_authtoken/admin_user':        value => $admin_user;
+    'keystone_authtoken/admin_password':    value => $admin_password, secret => true;
+  }
+
+  if $auth_admin_prefix {
+    validate_re($auth_admin_prefix, '^(/.+[^/])?$')
+    ironic_config {
+      'keystone_authtoken/auth_admin_prefix': value => $auth_admin_prefix;
+    }
+  } else {
+    ironic_config {
+      'keystone_authtoken/auth_admin_prefix': ensure => absent;
+    }
+  }
+
 
 }
