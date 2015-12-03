52cf1ca6c1e0c38a1a1616d45d630f2efd6e46e6
Denis Egorenko <degorenko@mirantis.com>
2015-12-03 19:57:38 +0300

    Remove qpid messaging support
    Qpid messaging driver is removed from Oslo.messaging [1]
    Therefore, we cannot support it anymore.
    
    [1] I4a9cba314c4a2f24307504fa7b5427424268b114
    
    Change-Id: I11ea3f58dc48b12dd6a536c3d56e57b7c48e6810

--- manifests/init.pp
+++ manifests/init.pp
@@ -119,21 +119,6 @@
 #   multiple RabbitMQ Brokers.
 #   Defaults to false
 #
-# [*qpid_hostname*]
-# [*qpid_port*]
-# [*qpid_username*]
-# [*qpid_password*]
-# [*qpid_heartbeat*]
-# [*qpid_protocol*]
-# [*qpid_tcp_nodelay*]
-# [*qpid_reconnect*]
-# [*qpid_reconnect_timeout*]
-# [*qpid_reconnect_limit*]
-# [*qpid_reconnect_interval*]
-# [*qpid_reconnect_interval_min*]
-# [*qpid_reconnect_interval_max*]
-#   (optional) various QPID options
-#
 # [*use_syslog*]
 #   (optional) Use syslog for logging
 #   Defaults to undef.
@@ -200,6 +185,22 @@
 #   Enable dbsync
 #   Defaults to true
 #
+# DEPRECATED PARAMETERS
+#
+# [*qpid_hostname*]
+# [*qpid_port*]
+# [*qpid_username*]
+# [*qpid_password*]
+# [*qpid_heartbeat*]
+# [*qpid_protocol*]
+# [*qpid_tcp_nodelay*]
+# [*qpid_reconnect*]
+# [*qpid_reconnect_timeout*]
+# [*qpid_reconnect_limit*]
+# [*qpid_reconnect_interval*]
+# [*qpid_reconnect_interval_min*]
+# [*qpid_reconnect_interval_max*]
+#
 class ironic (
   $enabled                     = true,
   $package_ensure              = 'present',
@@ -227,19 +228,6 @@ class ironic (
   $kombu_ssl_keyfile           = undef,
   $kombu_ssl_version           = 'TLSv1',
   $amqp_durable_queues         = false,
-  $qpid_hostname               = 'localhost',
-  $qpid_port                   = '5672',
-  $qpid_username               = 'guest',
-  $qpid_password               = 'guest',
-  $qpid_heartbeat              = 60,
-  $qpid_protocol               = 'tcp',
-  $qpid_tcp_nodelay            = true,
-  $qpid_reconnect              = true,
-  $qpid_reconnect_timeout      = 0,
-  $qpid_reconnect_limit        = 0,
-  $qpid_reconnect_interval_min = 0,
-  $qpid_reconnect_interval_max = 0,
-  $qpid_reconnect_interval     = 0,
   $use_syslog                  = false,
   $log_facility                = 'LOG_USER',
   $database_connection         = undef,
@@ -256,6 +244,19 @@ class ironic (
   $sync_db                     = true,
   # DEPRECATED PARAMETERS
   $rabbit_user                 = undef,
+  $qpid_hostname               = undef,
+  $qpid_port                   = undef,
+  $qpid_username               = undef,
+  $qpid_password               = undef,
+  $qpid_heartbeat              = undef,
+  $qpid_protocol               = undef,
+  $qpid_tcp_nodelay            = undef,
+  $qpid_reconnect              = undef,
+  $qpid_reconnect_timeout      = undef,
+  $qpid_reconnect_limit        = undef,
+  $qpid_reconnect_interval_min = undef,
+  $qpid_reconnect_interval_max = undef,
+  $qpid_reconnect_interval     = undef,
 ) {
 
   include ::ironic::logging
@@ -369,21 +370,7 @@ class ironic (
   }
 
   if $rpc_backend == 'ironic.openstack.common.rpc.impl_qpid' or $rpc_backend == 'qpid' {
-    ironic_config {
-      'oslo_messaging_qpid/qpid_hostname':               value => $qpid_hostname;
-      'oslo_messaging_qpid/qpid_port':                   value => $qpid_port;
-      'oslo_messaging_qpid/qpid_username':               value => $qpid_username;
-      'oslo_messaging_qpid/qpid_password':               value => $qpid_password, secret => true;
-      'oslo_messaging_qpid/qpid_heartbeat':              value => $qpid_heartbeat;
-      'oslo_messaging_qpid/qpid_protocol':               value => $qpid_protocol;
-      'oslo_messaging_qpid/qpid_tcp_nodelay':            value => $qpid_tcp_nodelay;
-      'oslo_messaging_qpid/qpid_reconnect':              value => $qpid_reconnect;
-      'oslo_messaging_qpid/qpid_reconnect_timeout':      value => $qpid_reconnect_timeout;
-      'oslo_messaging_qpid/qpid_reconnect_limit':        value => $qpid_reconnect_limit;
-      'oslo_messaging_qpid/qpid_reconnect_interval_min': value => $qpid_reconnect_interval_min;
-      'oslo_messaging_qpid/qpid_reconnect_interval_max': value => $qpid_reconnect_interval_max;
-      'oslo_messaging_qpid/qpid_reconnect_interval':     value => $qpid_reconnect_interval;
-    }
+    warning('Qpid driver is removed from Oslo.messaging in the Mitaka release')
   }
 
 }
