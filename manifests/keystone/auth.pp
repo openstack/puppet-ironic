23975a0d83ea90748ec83d94197c1ee9974ca2b2
Emilien Macchi <emilien@redhat.com>
2015-12-21 20:10:34 +0100

    Make Keystone_endpoint match service by name/type
    Since a chance in puppet-keystone (1], we now match an endpoint with a
    service name/type.
    )
    [1] http://git.openstack.org/cgit/openstack/puppet-keystone/commit/?id=0a4e06abb0f5b3f324464ff5219d2885816311ce
    
    Depends-On: I6e411d8f81c7ae5c768d85a236c0942d265c74dd
    Change-Id: Ie62c22c1bbfd1fbd224cc4858cdf3ffaa2e6a8ea
    Closes-Bug: #1528308
    (cherry picked from commit 1777e6c0316b6643d27e2e7efcc2bd5b26c01bd4)

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -193,7 +193,7 @@ class ironic::keystone::auth (
     Keystone_user_role["${auth_name}@${tenant}"] ~> Service <| name == 'ironic-server' |>
   }
 
-  Keystone_endpoint["${region}/${real_service_name}"]  ~> Service <| name == 'ironic-server' |>
+  Keystone_endpoint["${region}/${real_service_name}::${service_type}"]  ~> Service <| name == 'ironic-server' |>
 
   keystone::resource::service_identity { $auth_name:
     configure_user      => $configure_user,
