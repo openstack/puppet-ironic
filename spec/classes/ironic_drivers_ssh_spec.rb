7b139119003b7001280c72a5ea5b432e430ac8ff
Emilien Macchi <emilien@redhat.com>
2016-07-26 11:21:43 -0400

    Allow to configure [ssh] section in ironic config
    Allow to configure libvirt_uri from [ssh] section.
    
    Change-Id: I5f38fed9ffbe1b2dad650a2b0dd6c6ce4c145e87

---
+++ spec/classes/ironic_drivers_ssh_spec.rb
@@ -0,0 +1,50 @@
+# Copyright 2016 Red Hat, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Unit tests for ironic::drivers::ipmi class
+#
+
+require 'spec_helper'
+
+describe 'ironic::drivers::ssh' do
+
+  shared_examples_for 'ironic driver ssh' do
+
+    context 'with default parameters' do
+      it { is_expected.to contain_ironic_config('ssh/libvirt_uri').with_value('<SERVICE DEFAULT>') }
+    end
+
+    context 'when overriding parameters' do
+      let :params do
+        { :libvirt_uri => 'qemu:///session' }
+      end
+
+      it { is_expected.to contain_ironic_config('ssh/libvirt_uri').with_value('qemu:///session') }
+    end
+
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+      it_configures 'ironic driver ssh'
+    end
+  end
+
+end
