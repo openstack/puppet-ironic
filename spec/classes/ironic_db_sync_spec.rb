openstack/puppet-ironic

commit bf00e679016b60b421a9af353dd09dcaef514926

Fix user in sync.pp
The bug[0] is fixed, so we can fix the user.

[0] https://bugs.launchpad.net/cloud-archive/+bug/1450942

Change-Id: I89bdeec3f9723605acad29b90328ad0479b15ffc

manifests/db/sync.pp
@@ -18,12 +18,7 @@ class ironic::db::sync(
   exec { 'ironic-dbsync':
     command     => "${::ironic::params::dbsync_command} ${extra_params}",
     path        => '/usr/bin',
-    # Ubuntu packaging is running dbsync command as root during ironic-common
-    # postinstall script so when Puppet tries to run dbsync again, it fails
-    # because it is run with ironic user.
-    # This is a temporary patch until it's changed in Packaging
-    # https://bugs.launchpad.net/cloud-archive/+bug/1450942
-    user        => 'root',
+    user        => 'ironic',
     refreshonly => true,
     try_sleep   => 5,
     tries       => 10,

spec/classes/ironic_db_sync_spec.rb
@@ -8,7 +8,7 @@ describe 'ironic::db::sync' do
       is_expected.to contain_exec('ironic-dbsync').with(
         :command     => 'ironic-dbsync --config-file /etc/ironic/ironic.conf ',
         :path        => '/usr/bin',
-        :user        => 'root',
+        :user        => 'ironic',
         :refreshonly => 'true',
         :try_sleep   => 5,
         :tries       => 10,
@@ -29,7 +29,7 @@ describe 'ironic::db::sync' do
         it { is_expected.to contain_exec('ironic-dbsync').with(
             :command     => 'ironic-dbsync --config-file /etc/ironic/ironic.conf --config-file /etc/ironic/ironic_01.conf',
             :path        => '/usr/bin',
-            :user        => 'root',
+            :user        => 'ironic',
             :refreshonly => true,
             :try_sleep   => 5,
             :tries       => 10,

commit 179690bb93d6924517d7b22706e2b004ca4fab75

ironic upgrade: change ownership for ironic-dbsync.log
Commit bf00e679016b60b421a9af353dd09dcaef514926 broke upgrades of existing
ironic deployments, as it tries running ironic-dbsync from "ironic" user,
while the existing ironic-dbsync.log file is owned by root.

This change adds creating a file resource with the correct ACLs before
running ironic-dbsync.

Change-Id: Ia3e8b7d8add933cd8a53f1c4e92dc3c4479009eb

manifests/db/sync.pp
@@ -15,6 +15,17 @@ class ironic::db::sync(
   include ::ironic::deps
   include ::ironic::params
 
+  # NOTE(dtantsur): previous ironic-dbsync was run as root. it will fail to run
+  # as "ironic" user, if there is an old log file owned by root. Let's fix it.
+  # To be removed in Rocky.
+  file { '/var/log/ironic/ironic-dbsync.log':
+    ensure  => 'present',
+    owner   => 'ironic',
+    group   => 'ironic',
+    # /var/log/ironic comes from ironic-common
+    require => Anchor['ironic::install::end']
+  }
+
   exec { 'ironic-dbsync':
     command     => "${::ironic::params::dbsync_command} ${extra_params}",
     path        => '/usr/bin',
@@ -29,5 +40,6 @@ class ironic::db::sync(
       Anchor['ironic::dbsync::begin']
     ],
     notify      => Anchor['ironic::dbsync::end'],
+    require     => File['/var/log/ironic/ironic-dbsync.log'],
   }
 }

releasenotes/notes/ironic-dbsync-3816e081122eb137.yaml
@@ -1,0 +1,5 @@
+---
+fixes:
+  - |
+    Fixes ownership of ``/var/log/ironic/ironic-dbsync.log`` before trying
+    to run ``ironic-dbsync``. This file was owned by *root* previously.

spec/classes/ironic_db_sync_spec.rb
@@ -18,6 +18,12 @@ describe 'ironic::db::sync' do
                          'Anchor[ironic::dbsync::begin]'],
         :notify      => 'Anchor[ironic::dbsync::end]',
       )
+
+      is_expected.to contain_file('/var/log/ironic/ironic-dbsync.log').with(
+        :ensure  => 'present',
+        :owner   => 'ironic',
+        :group   => 'ironic',
+      )
     end
 
     describe "overriding extra_params" do