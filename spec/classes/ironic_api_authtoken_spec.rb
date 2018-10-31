29451f9f05b8e1ee98fe1a8c1355359b99b2b19a
Daniel Abad <d.abad@cern.ch>
2018-10-31 14:12:20 +0100

    Expose service_token_roles params
    Expose the [keystone_authtoken]/service_token_roles
    
    Depends-On: https://review.opendev.org/#/c/654648/
    Change-Id: I4bcc921347adf21f6eaf61af97f3d856c5217820

--- spec/classes/ironic_api_authtoken_spec.rb
+++ spec/classes/ironic_api_authtoken_spec.rb
@@ -42,6 +42,8 @@ describe 'ironic::api::authtoken' do
         is_expected.to contain_ironic_config('keystone_authtoken/memcached_servers').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_ironic_config('keystone_authtoken/region_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_ironic_config('keystone_authtoken/token_cache_time').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_ironic_config('keystone_authtoken/service_token_roles').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_ironic_config('keystone_authtoken/service_token_roles_required').with_value('<SERVICE DEFAULT>')
       end
     end
 
@@ -80,6 +82,8 @@ describe 'ironic::api::authtoken' do
           :manage_memcache_package        => true,
           :region_name                    => 'region2',
           :token_cache_time               => '301',
+          :service_token_roles            => ['service'],
+          :service_token_roles_required   => true,
         })
       end
 
@@ -115,6 +119,8 @@ describe 'ironic::api::authtoken' do
         is_expected.to contain_ironic_config('keystone_authtoken/memcached_servers').with_value('memcached01:11211,memcached02:11211')
         is_expected.to contain_ironic_config('keystone_authtoken/region_name').with_value(params[:region_name])
         is_expected.to contain_ironic_config('keystone_authtoken/token_cache_time').with_value(params[:token_cache_time])
+        is_expected.to contain_ironic_config('keystone_authtoken/service_token_roles').with_value(params[:service_token_roles])
+        is_expected.to contain_ironic_config('keystone_authtoken/service_token_roles_required').with_value(params[:service_token_roles_required])
       end
 
       it 'installs python memcache package' do
