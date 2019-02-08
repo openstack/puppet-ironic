13156df4c9f173cbfbeb5d3036de86002ac22067
Tobias Urdin <tobias.urdin@binero.se>
2019-02-08 12:13:37 +0100

    Use puppet 4 compatible mysql functions
    These was introduced in 6.0.0 and is required to
    support later version of puppetlabs-mysql.
    
    Change-Id: I14be6631aa1a4d0c9e21e58e661ddd8a563c86dd

--- manifests/inspector/db/mysql.pp
+++ manifests/inspector/db/mysql.pp
@@ -56,7 +56,7 @@ class ironic::inspector::db::mysql (
 
   ::openstacklib::db::mysql { 'ironic-inspector':
     user          => $user,
-    password_hash => mysql_password($password),
+    password_hash => mysql::password($password),
     dbname        => $dbname,
     host          => $host,
     charset       => $charset,
