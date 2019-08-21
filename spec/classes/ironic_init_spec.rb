486e62796ec2d6d2dcc2221f3e37ae5bfdb41549
ZhongShengping <chdzsp@163.com>
2019-08-21 14:21:01 +0800

    Introduce the new rabbit_heartbeat_in_pthread option
    oslo.messaging RabbitMQ driver have now a new option that allow user to
    run the RabbitMQ heartbeat over a native python thread.
    
    These change allow user to use this new option.
    
    Change-Id: I768827ca462e25fcb0571a3b5cb5e5eca615cc08
    Closes-Bug: #1840868

--- spec/classes/ironic_init_spec.rb
+++ spec/classes/ironic_init_spec.rb
@@ -111,6 +111,7 @@ describe 'ironic' do
       is_expected.to contain_ironic_config('DEFAULT/default_resource_class').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_ironic_config('DEFAULT/notification_level').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_ironic_config('DEFAULT/versioned_notifications_topics').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_ironic_config('oslo_messaging_rabbit/heartbeat_in_pthread').with_value('<SERVICE DEFAULT>')
     end
   end
 
