106ef76ac1d800729e7dbb7c830dc3f01a673bc3
Takashi Kajinami <tkajinam@redhat.com>
2020-04-12 22:21:38 +0900

    Add ironic::nova class
    ... to configure connection for nova service.
    
    Change-Id: If0826cd647a72d3cd4391c075878eba2a2c0a94f

---
+++ manifests/nova.pp
@@ -0,0 +1,81 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: ironic::nova
+#
+# [*auth_type*]
+#   The authentication plugin to use when connecting to nova.
+#   Defaults to 'password'
+#
+# [*auth_url*]
+#   The address of the keystone api endpoint.
+#   Defaults to $::os_service_default
+#
+# [*project_name*]
+#   The Keystone project name.
+#   Defaults to 'services'
+#
+# [*username*]
+#   The admin username for ironic to connect to nova.
+#   Defaults to 'ironic'.
+#
+# [*password*]
+#   The admin password for ironic to connect to nova.
+#   Defaults to $::os_service_default
+#
+# [*user_domain_name*]
+#   The name of user's domain (required for Identity V3).
+#   Defaults to 'Default'
+#
+# [*project_domain_name*]
+#   The name of project's domain (required for Identity V3).
+#   Defaults to 'Default'
+#
+# [*region_name*]
+#   (optional) Region name for connecting to nova in admin context
+#   through the OpenStack Identity service.
+#   Defaults to $::os_service_default
+#
+# [*endpoint_override*]
+#   The endpoint URL for requests for this client
+#   Defaults to $::os_service_default
+#
+# [*send_power_notifications*]
+#   Enable the support for power state change callbacks to nova.
+#   Defaults to $::os_service_default
+#
+class ironic::nova (
+  $auth_type                = 'password',
+  $auth_url                 = $::os_service_default,
+  $project_name             = 'services',
+  $username                 = 'ironic',
+  $password                 = $::os_service_default,
+  $user_domain_name         = 'Default',
+  $project_domain_name      = 'Default',
+  $region_name              = $::os_service_default,
+  $endpoint_override        = $::os_service_default,
+  $send_power_notifications = $::os_service_default,
+) {
+
+  ironic_config {
+    'nova/auth_type':                value => $auth_type;
+    'nova/username':                 value => $username;
+    'nova/password':                 value => $password, secret => true;
+    'nova/auth_url':                 value => $auth_url;
+    'nova/project_name':             value => $project_name;
+    'nova/user_domain_name':         value => $user_domain_name;
+    'nova/project_domain_name':      value => $project_domain_name;
+    'nova/region_name':              value => $region_name;
+    'nova/endpoint_override':        value => $endpoint_override;
+    'nova/send_power_notifications': value => $send_power_notifications;
+  }
+}
---
+++ spec/classes/ironic_nova_spec.rb
@@ -0,0 +1,93 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Unit tests for ironic::nova
+#
+
+require 'spec_helper'
+
+describe 'ironic::nova' do
+
+  let :default_params do
+    { :auth_type    => 'password',
+      :project_name => 'services',
+      :username     => 'ironic',
+    }
+  end
+
+  let :params do
+    {}
+  end
+
+  shared_examples_for 'ironic nova configuration' do
+    let :p do
+      default_params.merge(params)
+    end
+
+    it 'configures ironic.conf' do
+      is_expected.to contain_ironic_config('nova/auth_type').with_value(p[:auth_type])
+      is_expected.to contain_ironic_config('nova/auth_url').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_ironic_config('nova/project_name').with_value(p[:project_name])
+      is_expected.to contain_ironic_config('nova/username').with_value(p[:username])
+      is_expected.to contain_ironic_config('nova/password').with_value('<SERVICE DEFAULT>').with_secret(true)
+      is_expected.to contain_ironic_config('nova/user_domain_name').with_value('Default')
+      is_expected.to contain_ironic_config('nova/project_domain_name').with_value('Default')
+      is_expected.to contain_ironic_config('nova/region_name').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_ironic_config('nova/endpoint_override').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_ironic_config('nova/send_power_notifications').with_value('<SERVICE DEFAULT>')
+    end
+
+    context 'when overriding parameters' do
+      before :each do
+        params.merge!(
+            :auth_type                => 'noauth',
+            :auth_url                 => 'http://example.com',
+            :project_name             => 'project1',
+            :username                 => 'admin',
+            :password                 => 'pa$$w0rd',
+            :user_domain_name         => 'NonDefault',
+            :project_domain_name      => 'NonDefault',
+            :region_name              => 'regionTwo',
+            :endpoint_override        => 'http://example2.com',
+            :send_power_notifications => false,
+        )
+      end
+
+      it 'should replace default parameter with new value' do
+        is_expected.to contain_ironic_config('nova/auth_type').with_value(p[:auth_type])
+        is_expected.to contain_ironic_config('nova/auth_url').with_value(p[:auth_url])
+        is_expected.to contain_ironic_config('nova/project_name').with_value(p[:project_name])
+        is_expected.to contain_ironic_config('nova/username').with_value(p[:username])
+        is_expected.to contain_ironic_config('nova/password').with_value(p[:password]).with_secret(true)
+        is_expected.to contain_ironic_config('nova/user_domain_name').with_value(p[:user_domain_name])
+        is_expected.to contain_ironic_config('nova/project_domain_name').with_value(p[:project_domain_name])
+        is_expected.to contain_ironic_config('nova/region_name').with_value(p[:region_name])
+        is_expected.to contain_ironic_config('nova/endpoint_override').with_value(p[:endpoint_override])
+        is_expected.to contain_ironic_config('nova/send_power_notifications').with_value(p[:send_power_notifications])
+      end
+    end
+
+  end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'ironic nova configuration'
+    end
+  end
+
+end
