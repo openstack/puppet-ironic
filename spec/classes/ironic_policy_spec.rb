dfe8cd47835e00cc6c0cdb13a91be1a2675bdd05
Takashi Kajinami <tkajinam@redhat.com>
2021-03-16 17:20:26 +0900

    Add support for the oslo_policy/enforce_scope parameter
    Depends-on: https://review.opendev.org/#/c/759008/
    Change-Id: Ia28b3de3c5061d0407591a1c9053292ca3e04fb2

--- spec/classes/ironic_policy_spec.rb
+++ spec/classes/ironic_policy_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'ironic::policy' do
-
-  shared_examples_for 'ironic policies' do
+  shared_examples 'ironic::policy' do
     let :params do
       {
-        :policy_path => '/etc/ironic/policy.yaml',
-        :policies    => {
+        :enforce_scope => false,
+        :policy_path   => '/etc/ironic/policy.yaml',
+        :policies      => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
             'value' => 'foo:bar'
@@ -24,20 +24,21 @@ describe 'ironic::policy' do
         :file_format => 'yaml',
       })
       is_expected.to contain_oslo__policy('ironic_config').with(
-        :policy_file => '/etc/ironic/policy.yaml',
+        :enforce_scope => false,
+        :policy_file   => '/etc/ironic/policy.yaml',
       )
     end
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'ironic policies'
+      it_behaves_like 'ironic::policy'
     end
   end
 end
