bb714ff15aff24476a512279e2f59b379197eb21
Takashi Kajinami <tkajinam@redhat.com>
2022-01-25 16:41:38 +0900

    Accept system scope credential for Nova API request
    Currently Ironic uses the user credential in [nova] section to use
    External Event API in Nova but this API is available only for system
    admin when SRBAC is enforced.
    
    This change allows usage of system-scoped credential instead of
    project-scoped one.
    
    Change-Id: Ib5020f4bd01a18580aa765248ea29e132862d57c

--- manifests/nova.pp
+++ manifests/nova.pp
@@ -40,6 +40,10 @@
 #   The name of project's domain (required for Identity V3).
 #   Defaults to 'Default'
 #
+# [*system_scope*]
+#   (Optional) Scope for system operations
+#   Defaults to $::os_service_default
+#
 # [*region_name*]
 #   (optional) Region name for connecting to nova in admin context
 #   through the OpenStack Identity service.
@@ -61,19 +65,31 @@ class ironic::nova (
   $password                 = $::os_service_default,
   $user_domain_name         = 'Default',
   $project_domain_name      = 'Default',
+  $system_scope             = $::os_service_default,
   $region_name              = $::os_service_default,
   $endpoint_override        = $::os_service_default,
   $send_power_notifications = $::os_service_default,
 ) {
+
+  include ironic::deps
+
+  if is_service_default($system_scope) {
+    $project_name_real = $project_name
+    $project_domain_name_real = $project_domain_name
+  } else {
+    $project_name_real = $::os_service_default
+    $project_domain_name_real = $::os_service_default
+  }
 
   ironic_config {
     'nova/auth_type':                value => $auth_type;
     'nova/username':                 value => $username;
     'nova/password':                 value => $password, secret => true;
     'nova/auth_url':                 value => $auth_url;
-    'nova/project_name':             value => $project_name;
+    'nova/project_name':             value => $project_name_real;
     'nova/user_domain_name':         value => $user_domain_name;
-    'nova/project_domain_name':      value => $project_domain_name;
+    'nova/project_domain_name':      value => $project_domain_name_real;
+    'nova/system_scope':             value => $system_scope;
     'nova/region_name':              value => $region_name;
     'nova/endpoint_override':        value => $endpoint_override;
     'nova/send_power_notifications': value => $send_power_notifications;
