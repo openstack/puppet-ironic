c317250f967016edf908feaafc9bf79629c7604f
Takashi Kajinami <tkajinam@redhat.com>
2022-05-06 20:47:58 +0900

    apache+mod_wsgi: Disable SSL by default
    During the previous cycle, a warning message was added to inform users
    of this change.
    
    Now the default value is updated so that SSL is disabled by default.
    
    Change-Id: If565fecd8919d0a34c0b6b3009b7623610e78065

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -38,7 +38,7 @@
 #
 #   [*ssl*]
 #     Use ssl ? (boolean)
-#     Optional. Defaults to true
+#     Optional. Defaults to false
 #
 #   [*workers*]
 #     Number of WSGI workers to spawn.
@@ -107,7 +107,7 @@ class ironic::wsgi::apache (
   $port                        = 6385,
   $bind_host                   = undef,
   $path                        = '/',
-  $ssl                         = undef,
+  $ssl                         = false,
   $workers                     = $::os_workers,
   $ssl_cert                    = undef,
   $ssl_key                     = undef,
@@ -126,11 +126,6 @@ class ironic::wsgi::apache (
   $vhost_custom_fragment       = undef,
 ) {
 
-  if $ssl == undef {
-    warning('Default of the ssl parameter will be changed in a future release')
-  }
-  $ssl_real = pick($ssl, true)
-
   include ironic::deps
   include ironic::params
 
@@ -143,7 +138,7 @@ class ironic::wsgi::apache (
     path                        => $path,
     priority                    => $priority,
     servername                  => $servername,
-    ssl                         => $ssl_real,
+    ssl                         => $ssl,
     ssl_ca                      => $ssl_ca,
     ssl_cert                    => $ssl_cert,
     ssl_certs_dir               => $ssl_certs_dir,
