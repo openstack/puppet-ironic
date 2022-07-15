f3693d3e881218c2853f37a0d0f253a097fe7056
Takashi Kajinami <tkajinam@redhat.com>
2022-07-15 11:44:50 +0900

    Fix wrong test description
    The value with a white space should be accepted instead of being
    rejected. This change fixes the wrong test case description and makes
    sure the description explains what is actually tested.
    
    Change-Id: I841df474bd0aa7a84f0b443d87d002c343971ebd

--- spec/unit/type/ironic_config_spec.rb
+++ spec/unit/type/ironic_config_spec.rb
@@ -33,7 +33,7 @@ describe 'Puppet::Type.type(:ironic_config)' do
     expect(@ironic_config[:value]).to eq(['bar'])
   end
 
-  it 'should not accept a value with whitespace' do
+  it 'should accept a value with whitespace' do
     @ironic_config[:value] = 'b ar'
     expect(@ironic_config[:value]).to eq(['b ar'])
   end
