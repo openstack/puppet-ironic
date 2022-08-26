62043acf72fdf0854d327dd692ed55644fbf24c3
Takashi Kajinami <tkajinam@redhat.com>
2022-08-26 15:56:30 +0900

    Add Apache WSGI logging parameters for pipe/syslog
    Add parameters for advanced logging configurations in Apache to
    support piped logging and support for syslog (via mod_syslog
    available in Apache >= 2.5.0)
    
    Co-Authored-By: Andy Botting <andy@andybotting.com>
    Change-Id: I55f899284edfe15c177e5253bb3e278d73955fe9

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -70,6 +70,14 @@
 #     The log file name for the virtualhost.
 #     Optional. Defaults to undef.
 #
+#   [*access_log_pipe*]
+#     Specifies a pipe where Apache sends access logs for the virtualhost.
+#     Optional. Defaults to undef.
+#
+#   [*access_log_syslog*]
+#     Sends the virtualhost access log messages to syslog.
+#     Optional. Defaults to undef.
+#
 #   [*access_log_format*]
 #     The log format for the virtualhost.
 #     Optional. Defaults to undef.
@@ -78,13 +86,21 @@
 #     The error log file name for the virtualhost.
 #     Optional. Defaults to undef.
 #
-# [*custom_wsgi_process_options*]
-#   (optional) gives you the opportunity to add custom process options or to
-#   overwrite the default options for the WSGI main process.
-#   eg. to use a virtual python environment for the WSGI process
-#   you could set it to:
-#   { python-path => '/my/python/virtualenv' }
-#   Defaults to {}
+#   [*error_log_pipe*]
+#     Specifies a pipe where Apache sends error logs for the virtualhost.
+#     Optional. Defaults to undef.
+#
+#   [*error_log_syslog*]
+#     Sends the virtualhost error log messages to syslog.
+#     Optional. Defaults to undef.
+#
+#   [*custom_wsgi_process_options*]
+#     (optional) gives you the opportunity to add custom process options or to
+#     overwrite the default options for the WSGI main process.
+#     eg. to use a virtual python environment for the WSGI process
+#     you could set it to:
+#     { python-path => '/my/python/virtualenv' }
+#     Defaults to {}
 #
 #   [*headers*]
 #     (optional) Headers for the vhost.
@@ -128,8 +144,12 @@ class ironic::wsgi::apache (
   $threads                     = 15,
   $priority                    = 10,
   $access_log_file             = undef,
+  $access_log_pipe             = undef,
+  $access_log_syslog           = undef,
   $access_log_format           = undef,
   $error_log_file              = undef,
+  $error_log_pipe              = undef,
+  $error_log_syslog            = undef,
   $custom_wsgi_process_options = {},
   $headers                     = undef,
   $request_headers             = undef,
@@ -168,9 +188,13 @@ class ironic::wsgi::apache (
     wsgi_script_source          => $::ironic::params::ironic_wsgi_script_source,
     headers                     => $headers,
     request_headers             => $request_headers,
+    custom_wsgi_process_options => $custom_wsgi_process_options,
     access_log_file             => $access_log_file,
+    access_log_pipe             => $access_log_pipe,
+    access_log_syslog           => $access_log_syslog,
     access_log_format           => $access_log_format,
     error_log_file              => $error_log_file,
-    custom_wsgi_process_options => $custom_wsgi_process_options,
+    error_log_pipe              => $error_log_pipe,
+    error_log_syslog            => $error_log_syslog,
   }
 }
