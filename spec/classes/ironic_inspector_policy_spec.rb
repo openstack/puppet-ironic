3bf5a779dfc99a10f54c1b0d8a97253ba7404969
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:50:24 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: I051bf72e078071eee7e76638a4b17b0f8dd80013
    (cherry picked from commit 7c5b921065f906dc1eeebcd26002e2098f8ff99f)
    (cherry picked from commit 34da932cbbdedae2400d044b1d67c967c64e7d63)

--- spec/classes/ironic_inspector_policy_spec.rb
+++ spec/classes/ironic_inspector_policy_spec.rb
@@ -32,6 +32,7 @@ describe 'ironic::inspector::policy' do
           :file_group   => 'ironic-inspector',
           :file_format  => 'yaml',
           :purge_config => false,
+          :tag          => 'ironic-inspector',
         )
         is_expected.to contain_oslo__policy('ironic_inspector_config').with(
           :enforce_scope        => false,
@@ -61,6 +62,7 @@ describe 'ironic::inspector::policy' do
           :file_group   => 'ironic-inspector',
           :file_format  => 'yaml',
           :purge_config => true,
+          :tag          => 'ironic-inspector',
         )
         is_expected.to contain_oslo__policy('ironic_inspector_config').with(
           :enforce_scope        => false,
