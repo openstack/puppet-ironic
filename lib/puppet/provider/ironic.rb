06e65b5fee75b6476427aa5377d22780592c4dd7
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-11-16 23:34:55 +0900

    Use core provider implementation to look up keystone resources
    ... so that the credentials in clouds.yaml file can be used if
    available instead of openrc.
    
    Also, the transformation from project name to project uuid is still
    needed so the transform_to method has been undeprecated.
    
    Change-Id: I0e0d42d92e8272f3f7faf809e07e599805bbedaa

--- lib/puppet/provider/ironic.rb
+++ lib/puppet/provider/ironic.rb
@@ -1,27 +1,10 @@
 require 'puppet/provider/openstack'
 require 'puppet/provider/openstack/auth'
-require 'puppet/provider/openstack/credentials'
 
-class Puppet::Provider::Ironic < Puppet::Provider
+class Puppet::Provider::Ironic < Puppet::Provider::Openstack
+  extend Puppet::Provider::Openstack::Auth
 
-end
-
-class Puppet::Provider::Ironic::OpenstackRequest
-  include Puppet::Provider::Openstack::Auth
-
-  def openstack_request(service, action, properties=nil, options={})
-    credentials = Puppet::Provider::Openstack::CredentialsV3.new
-    openstack = Puppet::Provider::Openstack
-
-    set_credentials(credentials, get_os_vars_from_env)
-      unless credentials.set?
-        credentials.unset
-        set_credentials(credentials, get_os_vars_from_rcfile(rc_filename))
-      end
-    unless credentials.set?
-        raise(Puppet::Error::OpenstackAuthInputError, 'Insufficient credentials to authenticate')
-    end
-
-    openstack.request(service, action, properties, credentials, options)
+  def self.system_request(service, actuon, properties=nil, options={})
+    self.request(service, action, properties, options, 'system')
   end
 end
--- lib/puppet/provider/ironic_config/openstackconfig.rb
+++ lib/puppet/provider/ironic_config/openstackconfig.rb
@@ -10,19 +10,17 @@ Puppet::Type.type(:ironic_config).provide(
   end
 
   def to_project_uuid(name)
-    warning('to_project_uuid is deprecated and will be removed in a future release.')
     properties = [name, '--column', 'id']
-    openstack = Puppet::Provider::Ironic::OpenstackRequest.new
-    res = openstack.openstack_request('project', 'show', properties)
+    ironic_provider = Puppet::Provider::Ironic
+    res = ironic_provider.system_request('project', 'show', properties)
     return "AUTH_#{res[:id]}"
   end
 
   def from_project_uuid(uuid)
-    warning('from_project_uuid is deprecated and will be removed in a future release.')
     uuid = uuid.sub('AUTH_','')
     properties = [uuid, '--column', 'name']
-    openstack = Puppet::Provider::Ironic::OpenstackRequest.new
-    res = openstack.openstack_request('project', 'show', properties)
+    ironic_provider = Puppet::Provider::Ironic
+    res = ironic_provider.system_request('project', 'show', properties)
     return res[:name]
   end
 end
