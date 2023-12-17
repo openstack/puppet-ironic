2eaa4e1aa0364d1015b41a242b36c728b180e9e0
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-12-18 01:45:23 +0900

    inspector: Prepare to use ironic-inspector user by default
    Currently even ironic-inspector services use the ironic keystone user
    though the ironic-inspector keystone user is created. This adds
    a warning message to make users aware of the future change to use
    the own service user.
    
    Change-Id: I62b7cf88496a811f4068244d7a3fb563a53aa1d6

--- manifests/inspector/authtoken.pp
+++ manifests/inspector/authtoken.pp
@@ -237,6 +237,10 @@ class ironic::inspector::authtoken(
 
   include ironic::deps
 
+  if $username == 'ironic' {
+    warning('The default username will be changed to ironic-inspector in a future release')
+  }
+
   keystone::resource::authtoken {
     'ironic_inspector_config':
       *                              => $params;
