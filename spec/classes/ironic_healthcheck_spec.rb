39c8e50eabd8932e7e2dc76576fe937052d731fc
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-01-17 15:07:03 +0900

    healthcheck: Expose allowed_source_ranges
    ... which was added to puppet-oslo recently.
    
    Depends-on: https://review.opendev.org/905557
    Change-Id: Ie089505d31cc5481e446d74fd299ffed6809d613

--- spec/classes/ironic_healthcheck_spec.rb
+++ spec/classes/ironic_healthcheck_spec.rb
@@ -15,6 +15,7 @@ describe 'ironic::healthcheck' do
         is_expected.to contain_oslo__healthcheck('ironic_config').with(
           :detailed              => '<SERVICE DEFAULT>',
           :backends              => '<SERVICE DEFAULT>',
+          :allowed_source_ranges => '<SERVICE DEFAULT>',
           :disable_by_file_path  => '<SERVICE DEFAULT>',
           :disable_by_file_paths => '<SERVICE DEFAULT>',
         )
@@ -27,6 +28,7 @@ describe 'ironic::healthcheck' do
           :enabled               => true,
           :detailed              => true,
           :backends              => ['disable_by_file'],
+          :allowed_source_ranges => ['10.0.0.0/24', '10.0.1.0/24'],
           :disable_by_file_path  => '/etc/ironic/healthcheck/disabled',
           :disable_by_file_paths => ['8042:/etc/ironic/healthcheck/disabled'],
         }
@@ -38,6 +40,7 @@ describe 'ironic::healthcheck' do
         is_expected.to contain_oslo__healthcheck('ironic_config').with(
           :detailed              => true,
           :backends              => ['disable_by_file'],
+          :allowed_source_ranges => ['10.0.0.0/24', '10.0.1.0/24'],
           :disable_by_file_path  => '/etc/ironic/healthcheck/disabled',
           :disable_by_file_paths => ['8042:/etc/ironic/healthcheck/disabled'],
         )
