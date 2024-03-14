28149003cd1a20bf824eb4e255ff7a9f2ac384af
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-03-14 09:31:04 +0900

    Expose rabbit_transient_quorum_queue
    Depends-on: https://review.opendev.org/911021
    Change-Id: I8501190b817e8f2da0941056d86c21cda48aeee0

--- manifests/inspector.pp
+++ manifests/inspector.pp
@@ -252,6 +252,10 @@
 #   (Optional) Use quorum queues in RabbitMQ.
 #   Defaults to $facts['os_service_default']
 #
+# [*rabbit_transient_quorum_queue*]
+#   (Optional) Use quorum queues for transients queues in RabbitMQ.
+#   Defaults to $facts['os_service_default']
+#
 # [*rabbit_quorum_delivery_limit*]
 #   (Optional) Each time a message is rdelivered to a consumer, a counter is
 #   incremented. Once the redelivery count exceeds the delivery limit
@@ -358,6 +362,7 @@ class ironic::inspector (
   $rabbit_heartbeat_in_pthread                = $facts['os_service_default'],
   $rabbit_ha_queues                           = $facts['os_service_default'],
   $rabbit_quorum_queue                        = $facts['os_service_default'],
+  $rabbit_transient_quorum_queue              = $facts['os_service_default'],
   $rabbit_quorum_delivery_limit               = $facts['os_service_default'],
   $rabbit_quorum_max_memory_length            = $facts['os_service_default'],
   $rabbit_quorum_max_memory_bytes             = $facts['os_service_default'],
@@ -502,6 +507,7 @@ class ironic::inspector (
     kombu_ssl_version               => $kombu_ssl_version,
     rabbit_ha_queues                => $rabbit_ha_queues,
     rabbit_quorum_queue             => $rabbit_quorum_queue,
+    rabbit_transient_quorum_queue   => $rabbit_transient_quorum_queue,
     rabbit_quorum_delivery_limit    => $rabbit_quorum_delivery_limit,
     rabbit_quorum_max_memory_length => $rabbit_quorum_max_memory_length,
     rabbit_quorum_max_memory_bytes  => $rabbit_quorum_max_memory_bytes,
