09cb1e7cb5e5b4bccd127ce786c45a1bd10b0aeb
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-05-05 00:02:26 +0900

    nova: Omit auth options when notification is disabled
    If send_power_notifications is false and the notification is disabled,
    ironic does not need to send requests to Nova. Thus password and
    the other auth options are not needed.
    
    Change-Id: I3f5071b85df029f5db535b2b0cf97edd317bc006

--- spec/classes/ironic_nova_spec.rb
+++ spec/classes/ironic_nova_spec.rb
@@ -17,28 +17,31 @@ require 'spec_helper'
 
 describe 'ironic::nova' do
 
-  let :params do
-    { :password => 'secret' }
-  end
-
   shared_examples_for 'ironic nova configuration' do
-    it 'configures ironic.conf' do
-      is_expected.to contain_ironic_config('nova/auth_type').with_value('password')
-      is_expected.to contain_ironic_config('nova/auth_url').with_value('http://127.0.0.1:5000')
-      is_expected.to contain_ironic_config('nova/project_name').with_value('services')
-      is_expected.to contain_ironic_config('nova/username').with_value('ironic')
-      is_expected.to contain_ironic_config('nova/password').with_value('secret').with_secret(true)
-      is_expected.to contain_ironic_config('nova/user_domain_name').with_value('Default')
-      is_expected.to contain_ironic_config('nova/project_domain_name').with_value('Default')
-      is_expected.to contain_ironic_config('nova/system_scope').with_value('<SERVICE DEFAULT>')
-      is_expected.to contain_ironic_config('nova/region_name').with_value('<SERVICE DEFAULT>')
-      is_expected.to contain_ironic_config('nova/endpoint_override').with_value('<SERVICE DEFAULT>')
-      is_expected.to contain_ironic_config('nova/send_power_notifications').with_value('<SERVICE DEFAULT>')
+    context 'with defaults' do
+      let :params do
+        { :password => 'secret' }
+      end
+
+      it 'configures ironic.conf' do
+        is_expected.to contain_ironic_config('nova/auth_type').with_value('password')
+        is_expected.to contain_ironic_config('nova/auth_url').with_value('http://127.0.0.1:5000')
+        is_expected.to contain_ironic_config('nova/project_name').with_value('services')
+        is_expected.to contain_ironic_config('nova/username').with_value('ironic')
+        is_expected.to contain_ironic_config('nova/password').with_value('secret').with_secret(true)
+        is_expected.to contain_ironic_config('nova/user_domain_name').with_value('Default')
+        is_expected.to contain_ironic_config('nova/project_domain_name').with_value('Default')
+        is_expected.to contain_ironic_config('nova/system_scope').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_ironic_config('nova/region_name').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_ironic_config('nova/endpoint_override').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_ironic_config('nova/send_power_notifications').with_value(true)
+      end
     end
 
     context 'when overriding parameters' do
-      before :each do
-        params.merge!(
+      let :params do
+        {
+          :password                 => 'secret',
           :auth_type                => 'noauth',
           :auth_url                 => 'http://example.com',
           :project_name             => 'project1',
@@ -47,8 +50,8 @@ describe 'ironic::nova' do
           :project_domain_name      => 'NonDefault',
           :region_name              => 'regionTwo',
           :endpoint_override        => 'http://example2.com',
-          :send_power_notifications => false,
-        )
+          :send_power_notifications => true,
+        }
       end
 
       it 'should replace default parameter with new value' do
@@ -66,16 +69,36 @@ describe 'ironic::nova' do
     end
 
     context 'when system_scope is set' do
-      before :each do
-        params.merge!(
+      let :params do
+        {
+          :password     => 'secret',
           :system_scope => 'all',
-        )
+        }
       end
 
       it 'configures system-scoped credential' do
         is_expected.to contain_ironic_config('nova/project_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_ironic_config('nova/project_domain_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_ironic_config('nova/system_scope').with_value('all')
+      end
+    end
+
+    context 'when send_power_notifications is false' do
+      let :params do
+        { :send_power_notifications => false }
+      end
+
+      it 'configures only send_power_notifications' do
+        is_expected.to contain_ironic_config('nova/auth_type').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/auth_url').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/project_name').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/username').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/user_domain_name').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/project_domain_name').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/region_name').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/system_scope').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/endpoint_override').with_ensure('absent')
+        is_expected.to contain_ironic_config('nova/send_power_notifications').with_value(false)
       end
     end
   end
