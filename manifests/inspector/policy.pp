9e598953a3946b2814f3a0e8b2e9e7a29706c077
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-09-27 23:59:59 +0900

    Import base parameter types for ::policy
    Import the parameter types from openstacklib::policy so that parameter
    types are validated at module level, instead of internal resource
    call.
    
    Change-Id: Iaefc78a1191630e13dd0b8a899e6281850744ee7
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/inspector/policy.pp
+++ manifests/inspector/policy.pp
@@ -46,13 +46,13 @@
 #    Defaults to false.
 #
 class ironic::inspector::policy (
-  $enforce_scope        = $facts['os_service_default'],
-  $enforce_new_defaults = $facts['os_service_default'],
-  Hash $policies        = {},
-  $policy_path          = '/etc/ironic-inspector/policy.yaml',
-  $policy_default_rule  = $facts['os_service_default'],
-  $policy_dirs          = $facts['os_service_default'],
-  Boolean $purge_config = false,
+  $enforce_scope                    = $facts['os_service_default'],
+  $enforce_new_defaults             = $facts['os_service_default'],
+  Openstacklib::Policies $policies  = {},
+  Stdlib::Absolutepath $policy_path = '/etc/ironic-inspector/policy.yaml',
+  $policy_default_rule              = $facts['os_service_default'],
+  $policy_dirs                      = $facts['os_service_default'],
+  Boolean $purge_config             = false,
 ) {
   include ironic::deps
   include ironic::params
@@ -64,7 +64,6 @@ class ironic::inspector::policy (
     file_group   => $ironic::params::inspector_group,
     file_format  => 'yaml',
     purge_config => $purge_config,
-    tag          => 'ironic-inspector',
   }
 
   create_resources('openstacklib::policy', { $policy_path => $policy_parameters })
