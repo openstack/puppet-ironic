a23cc8a4a2b1625df672d0bbbef5d6322b50cd57
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-09-09 11:45:36 +0900

    Deprecate support for [database] mysql_enable_nbd
    Depends-on: https://review.opendev.org/1004735
    Change-Id: I6babebd23b4bf08e83288bf6b90f22cb18601063
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/db.pp
+++ manifests/db.pp
@@ -38,10 +38,12 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $facts['os_service_default']
 #
+# DEPRECATED PARAMETERS
+#
 # [*mysql_enable_ndb*]
 #   (Optional) If True, transparently enables support for handling MySQL
 #   Cluster (NDB).
-#   Defaults to $facts['os_service_default']
+#   Defaults to undef
 #
 class ironic::db (
   $database_connection              = 'sqlite:////var/lib/ironic/ovs.sqlite',
@@ -52,7 +54,8 @@ class ironic::db (
   $database_max_overflow            = $facts['os_service_default'],
   $database_db_max_retries          = $facts['os_service_default'],
   $database_pool_timeout            = $facts['os_service_default'],
-  $mysql_enable_ndb                 = $facts['os_service_default'],
+  # DEPRECATED PARAMETERS
+  $mysql_enable_ndb                 = undef,
 ) {
   include ironic::deps
 
